aa5781b681797402317feb8c49d0c707e9ebcdb8
semen.mokrov <semen.mokrov@gmail.com>
2023-04-16 00:36:31 +0300

    Mini refactor

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,58 +7,75 @@ import model.Network;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class Client {
+    private static ClientManager clientManager;
+    private static FileListResponseType fileListResponseType;
+
+
+    private static Network getNetworkFromParams(String address) throws UnknownHostException {
+        String[] addressParams = address.split(":");
+        return new Network(addressParams[0], Integer.parseInt(addressParams[1]));
+    }
+
+    private static void printFileList() {
+        System.out.println("File List: ");
+        for (FileDescriptor file : fileListResponseType.getFileDescriptors()) {
+            System.out.println(file.getFile_id() + " - " + file.getFile_name());
+        }
+    }
+
+    private static int scanFileId() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        final int fileId = scanner.nextInt();
+        System.out.println("File " + fileId + " has been selected. Getting the size information...");
+        return fileId;
+    }
+
+    private static long getFileSize(int fileId) throws IOException {
+        try {
+            long size = clientManager.getFileSize(fileId);
+            System.out.println("File " + fileId + " is " + size + " bytes. Starting to download...");
+            return size;
+        } catch (IOException e) {
+            throw new IOException("Can't get file size");
+        }
+    }
+
     //how to tell start and end bytes.
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
             throw new IllegalArgumentException("ip:port is mandatory");
         }
-        ClientManager clientManager;
-        FileListResponseType fileListResponseType;
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-            String[] adr1 = args[0].split(":");
-            String ip1 = adr1[0];
-            int port1 = Integer.parseInt(adr1[1]);
-
-            String[] adr2 = args[1].split(":");
-            String ip2 = adr2[0];
-            int port2 = Integer.parseInt(adr2[1]);
+            final Network firstNetwork = getNetworkFromParams(args[0]);
+            final Network secondNetwork = getNetworkFromParams(args[1]);
 
             //TODO: Should we keep looping until 1 port gives datalist o try both and then quit?
             // this also applies to getting size etc, idk if we should consider this or not
 
             try {
-                clientManager = new ClientManager(new Network(ip1, port1));
-                fileListResponseType=clientManager.getFileList();
-            }catch (Exception e){
+                clientManager = new ClientManager(firstNetwork);
+                fileListResponseType = clientManager.getFileList();
+            } catch (IOException e) {
                 try {
-                    clientManager = new ClientManager(new Network(ip2, port2));
+                    clientManager = new ClientManager(secondNetwork);
                     fileListResponseType = clientManager.getFileList();
-                }catch (Exception x){
+                } catch (IOException x) {
                     System.out.println("Cannot create connection to the server. Quiting...");
                     break;
                 }
             }
 
-            System.out.println("File List: ");
-            for (FileDescriptor file : fileListResponseType.getFileDescriptors()) {
-                System.out.println(file.getFile_id() + " - " + file.getFile_name());
-            }
-
-            System.out.println("Enter a number:");
-            int fileId = scanner.nextInt();
-
-
-            System.out.println("File " + fileId + " has been selected. Getting the size information...");
-            long size = clientManager.getFileSize(fileId);
-            System.out.println("File " + fileId + " is " + size + " bytes. Starting to download...");
-
+            printFileList();
+            int fileId = scanFileId();
+            long size = getFileSize(fileId);
 
             long startByte = 1;
             boolean switch_ = true;
@@ -69,8 +86,8 @@ public class Client {
 
             while (!downloadFinished) {
                 clientManager = switch_ ?
-                        new ClientManager(new Network(ip1, port1)) :
-                        new ClientManager(new Network(ip2, port2));
+                        new ClientManager(firstNetwork) :
+                        new ClientManager(secondNetwork);
 
                 ClientManager finalClientManager = clientManager;
                 long finalStartByte = startByte;
@@ -95,12 +112,11 @@ public class Client {
 
                 }
 
-                if(response.isEmpty()){
+                if (response.isEmpty()) {
                     System.out.println("Switching!!!!!!!");
-                    switch_ = switch_ ? false: true;
+                    switch_ = switch_ ? false : true;
 
-                }
-                else if ((response.get(response.size() - 1).getEnd_byte()) < size) {
+                } else if ((response.get(response.size() - 1).getEnd_byte()) < size) {
                     System.out.println("Switching!!!!!!!");
                     startByte = response.get(response.size() - 1).getEnd_byte() + 1;
                     switch_ = false;
@@ -134,7 +150,7 @@ public class Client {
             for (int i = 0; i < mdBytes.length; i++) {
                 md5.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16).substring(1));
             }
-            System.out.println("File "+fileId + elapsedTime + " ms. The md5 hash is " + md5);
+            System.out.println("File " + fileId + elapsedTime + " ms. The md5 hash is " + md5);
             System.out.println();
         }
     }
